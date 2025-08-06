23f8e356c24b432310fb28e0ce80b3717fdee6af
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-08-06 16:11:45 +0530

    Basic Setup for Admin

---
+++ admin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <h1 class="text-[15px] font-bold bg-green-400 text-center flex flex-col justify-center items-center">
+      Hello world! 
+      <p className='text-[25px] font-semibold bg-indigo-500 inline'>Ayush Sharma</p>
+    </h1>
+  )
+}
+
+export default App
