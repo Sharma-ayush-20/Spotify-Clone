dd29227c1eb6b0bb68b87c79bc6d7eab6a3cca44
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-08-07 21:38:48 +0530

    Fetch song data in admin panel

--- admin/src/pages/ListSong.jsx
+++ admin/src/pages/ListSong.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify';
+import { url } from '../App';
 
 function ListSong() {
+
+  const [data, setData] = useState([]);
+
+  const fetchSongs = async () => {
+    try {
+      const response = await axios.get(`${url}/api/song/list`);
+      
+        if(response.data.success){
+          setData(response.data.message);
+        }
+      
+    } catch (error) {
+      toast.error("Error Occured");
+    }
+  }
+
+  useEffect(() => {
+    fetchSongs();
+  }, [])
+
   return (
-    <div>ListSong</div>
+    <div>
+      <p>All Songs List</p>
+      <br />
+      <div>
+        {/* header  */}
+          <div className='sm:grid hidden grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center p-3 gap-2.5 border border-gray-300 text-sm mr-5 bg-gray-100'>
+            <b>Image</b>
+            <b>Name</b>
+            <b>Album</b>
+            <b>Duration</b>
+            <b>Action</b>
+          </div>
+
+        {
+          data.map((item, index) => {
+            return (
+              <div key={index} className='grid grid-cols-[1fr_1fr_1fr] sm:grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5'>
+                <img src={item.image} alt="" className='w-12'/>
+                <p>{item.name}</p>
+                <p>{item.album}</p>
+                <p>{item.duration}</p>
+                <p className='cursor-pointer'>X</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
   )
 }
 
